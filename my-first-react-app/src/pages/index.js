86625b091bd183288b506d9bb66aef3f731a7f49
Jonathan Tang <jonathan.tang@myunidays.com>
2018-07-25 07:12:07 +0100

    Change content of default index page
    Signed-off-by: Jonathan Tang <jonathan.tang@myunidays.com>

--- my-first-react-app/src/pages/index.js
+++ my-first-react-app/src/pages/index.js
@@ -3,9 +3,8 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Welcome to my website!</h1>
+    <p>Complete courses about the best tools and design</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )

e577ac2db1a6c533a7ffb3ce53fb215c288b8a0d
Jonathan Tang <jonathan.tang@myunidays.com>
2018-07-25 07:40:34 +0100

    Remove default inline css and refactor existing content into Hero div group

--- my-first-react-app/src/pages/index.js
+++ my-first-react-app/src/pages/index.js
@@ -3,9 +3,13 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Welcome to my website!</h1>
-    <p>Complete courses about the best tools and design</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Welcome to my website!</h1>
+        <p>Complete courses about the best tools and design</p>
+        <Link to="/page-2/">Go to page 2</Link>
+      </div>
+    </div>
   </div>
 )
 

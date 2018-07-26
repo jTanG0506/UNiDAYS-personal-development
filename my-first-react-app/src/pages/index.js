5310ec18b627617c386ae4a37b8fc207f7334153
Jonathan Tang <jonathan.tang@myunidays.com>
2018-07-26 17:57:17 +0100

    Change text content to match course content

--- my-first-react-app/src/pages/index.js
+++ my-first-react-app/src/pages/index.js
@@ -5,9 +5,9 @@ const IndexPage = () => (
   <div>
     <div className="Hero">
       <div className="HeroGroup">
-        <h1>Welcome to my website!</h1>
-        <p>Complete courses about the best tools and design</p>
-        <Link to="/page-2/">Go to page 2</Link>
+        <h1>Learn to design and code React apps</h1>
+        <p>Complete courses about the best tools and design systems. Prototype and build apps with React with Swift.</p>
+        <Link to="/page-2/">WATCH THE VIDEO</Link>
       </div>
     </div>
   </div>

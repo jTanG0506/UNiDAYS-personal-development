0c9515f66d540c068679a633aa5dbd3e2e2eb764
Jonathan Tang <jonathan.tang@myunidays.com>
2018-07-31 07:16:08 +0100

    Add line break to increase title readability

--- my-first-react-app/src/pages/index.js
+++ my-first-react-app/src/pages/index.js
@@ -5,7 +5,7 @@ const IndexPage = () => (
   <div>
     <div className="Hero">
       <div className="HeroGroup">
-        <h1>Learn to design and code React apps</h1>
+        <h1>Learn to <br />design and code React apps</h1>
         <p>Complete courses about the best tools and design systems. Prototype and build apps with React with Swift.</p>
         <Link to="/page-2/">WATCH THE VIDEO</Link>
       </div>

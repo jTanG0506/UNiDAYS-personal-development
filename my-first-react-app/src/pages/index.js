3c96720e44af704ae2f3bc6d42d8933438ad366b
Jonathan Tang <jonathan.tang@myunidays.com>
2018-07-31 07:58:40 +0100

    Add logos to Hero section

--- my-first-react-app/src/pages/index.js
+++ my-first-react-app/src/pages/index.js
@@ -8,7 +8,15 @@ const IndexPage = () => (
         <h1>Learn to <br />design and code React apps</h1>
         <p>Complete courses about the best tools and design systems. Prototype and build apps with React with Swift.</p>
         <Link to="/page-2/">WATCH THE VIDEO</Link>
-        <svg width="100%" height="172" viewBox="0 0 100% 172" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <div className="Logos">
+          <img src={require('../images/logo-sketch.png')} width="50"/>
+          <img src={require('../images/logo-figma.png')} width="50"/>
+          <img src={require('../images/logo-studio.png')} width="50"/>
+          <img src={require('../images/logo-framer.png')} width="50"/>
+          <img src={require('../images/logo-react.png')} width="50"/>
+          <img src={require('../images/logo-swift.png')} width="50"/>
+        </div>
+        <svg width="100%" height="172" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path fill="white">
             <animate repeatCount="indefinite" fill="freeze" attributeName="d" dur="10s"
             values="M0 25.9086C277 84.5821 433 65.736 720 25.9086C934.818 -3.9019 1214.06 -5.23669 1442 8.06597C2079 45.2421 2208 63.5007 2560 25.9088V171.91L0 171.91V25.9086Z;

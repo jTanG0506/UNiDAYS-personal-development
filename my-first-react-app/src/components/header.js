30e3db25a9279d00c0095186f2758f73362cb809
Jonathan Tang <jonathan.tang@myunidays.com>
2018-08-01 19:13:12 +0100

    Convert Header into a stateful component and change appearance of header upon scroll

--- my-first-react-app/src/components/header.js
+++ my-first-react-app/src/components/header.js
@@ -2,16 +2,42 @@ import React from 'react'
 import Link from 'gatsby-link'
 import './Header.css'
 
-const Header = ({ siteTitle }) => (
-  <div className="Header">
-    <div className="HeaderGroup">
-      <Link to="/"><img src={require('../images/logo-designcode.svg')} width="30" /></Link>
-      <Link to="/courses">Courses</Link>
-      <Link to="/courses">Downloads</Link>
-      <Link to="/courses">Workshops</Link>
-      <Link to="/courses"><button>Buy</button></Link>
-    </div>
-  </div>
-)
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      hasScrolled: false
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+  handleScroll = (event) => {
+    const scrollTop = window.pageYOffset
+
+    if (scrollTop > 50) {
+      this.setState({ hasScrolled: true })
+    } else {
+      this.setState({ hasScrolled: false })
+    }
+  }
+
+  render() {
+    return (
+      <div className={this.state.hasScrolled ? 'Header HeaderScrolled' : 'Header'}>
+        <div className="HeaderGroup">
+          <Link to="/"><img src={require('../images/logo-designcode.svg')} width="30" /></Link>
+          <Link to="/courses">Courses</Link>
+          <Link to="/courses">Downloads</Link>
+          <Link to="/courses">Workshops</Link>
+          <Link to="/courses"><button>Buy</button></Link>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Header

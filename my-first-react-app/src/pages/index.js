8a922287c5073cbdc969517ea294cfee55e6f604
Jonathan Tang <jonathan.tang@myunidays.com>
2018-08-02 06:46:29 +0100

    Use Cell component in index and create SectionCaption and SectionCellGroup

--- my-first-react-app/src/pages/index.js
+++ my-first-react-app/src/pages/index.js
@@ -1,9 +1,32 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import styled from 'styled-components'
 import Card from '../components/Card';
 import Section from '../components/Section';
 import Wave from '../components/Wave';
+import Cell from '../components/Cell';
 import staticdata from '../../static-data.json'
+
+const SectionCaption = styled.p`
+  font-weight: 600;
+  font-size: 18px;
+  text-transform: uppercase;
+  color: #94A4BA;
+  text-align: center;
+`
+
+const SectionCellGroup = styled.div`
+  max-width: 800px;
+  margin: 0 auto 100px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-column-gap: 20px;
+  padding: 0 20px;
+
+  @media (max-width: 800px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
+`
 
 const IndexPage = () => (
   <div>
@@ -59,6 +82,15 @@ const IndexPage = () => (
       dynamic data with Contentful and deploying your
       site with Netlify."
     />
+    <SectionCaption>12 sections - 6 hours</SectionCaption>
+    <SectionCellGroup>
+      {staticdata.cells.map(cell => (
+        <Cell
+          title={cell.title}
+          image={cell.image}>
+        </Cell>
+      ))}
+    </SectionCellGroup>
   </div>
 )
 

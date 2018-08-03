c4519b594e59fc71dfa31725fa27f2cdfed0b4ba
Jonathan Tang <jonathan.tang@myunidays.com>
2018-08-03 08:00:28 +0100

    Update config file to setup contentful access

--- my-first-react-app/gatsby-config.js
+++ my-first-react-app/gatsby-config.js
@@ -1,8 +1,19 @@
+var secrets = require('./secrets.json');
+
 module.exports = {
   siteMetadata: {
     title: 'Jonathan Tang',
     description: 'Complete courses about the best tools and design systems. Prototype and build apps with React and Swift. 60 hours of video content and resource materials. No coding experience required.',
     keywords: 'react course, react for designers, ios development, sketch app, swift app course, arkit 2, after effects, create sketch plugin'
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: secrets.spaceId,
+        accessToken: secrets.contentDeliveryAPIKey
+      }
+    }
+  ],
 }

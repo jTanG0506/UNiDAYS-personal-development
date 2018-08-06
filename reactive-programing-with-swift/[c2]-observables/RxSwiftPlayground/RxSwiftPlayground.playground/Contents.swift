8a1bbe822cb02f0a631cc7bafefac07423c07f7a
Jonathan Tang <jonathan.tang@myunidays.com>
2018-08-06 17:22:10 +0100

    Create an observable using the create operator

--- reactive-programing-with-swift/[c2]-observables/RxSwiftPlayground/RxSwiftPlayground.playground/Contents.swift
+++ reactive-programing-with-swift/[c2]-observables/RxSwiftPlayground/RxSwiftPlayground.playground/Contents.swift
@@ -69,3 +69,25 @@ example(of: "DisposeBag") {
         print($0)
     }.disposed(by: disposeBag)
 }
+
+example(of: "create") {
+    
+    enum MyError: Error {
+        case anError
+    }
+    
+    let disposeBag = DisposeBag()
+    
+    Observable<String>.create{ observer -> Disposable in
+        observer.onNext("1")
+        observer.onError(MyError.anError)
+        observer.onCompleted()
+        observer.onNext("?")
+        return Disposables.create()
+    }.subscribe(
+        onNext: { print($0) },
+        onError: { print($0) },
+        onCompleted: { print("Completed") },
+        onDisposed: { print("Disposed") }
+    ).disposed(by: disposeBag)
+}

de83416cea11a69f1bc9ce6545bcdb8320ca7fbf
Jonathan Tang <jonathan.tang@myunidays.com>
2018-08-06 16:49:43 +0100

    Create simple observables and experiment with subscribe operator

---
+++ reactive-programing-with-swift/[c2]-observables/RxSwiftPlayground/RxSwiftPlayground.playground/Contents.swift
@@ -0,0 +1,57 @@
+import RxSwift
+
+example(of: "just, of, from") {
+    let one = 1
+    let two = 2
+    let three = 3
+    
+    let observable: Observable<Int> = Observable<Int>.just(1)
+    let observable2 = Observable.of(one, two, three)
+    let observable3 = Observable.of([one, two, three])
+    let observable4 = Observable.from([one, two, three])
+}
+
+example(of: "subscribe") {
+    let one = 1
+    let two = 2
+    let three = 3
+    
+    let observable = Observable.of(one, two, three)
+    
+    observable.subscribe(onNext: { element in
+        print(element)
+    })
+}
+
+example(of: "empty") {
+    let observable = Observable<Void>.empty()
+    
+    observable.subscribe(onNext: { (element) in
+        print(element)
+    }, onCompleted: {
+        print("Completed")
+    })
+}
+
+example(of: "never") {
+    let observable = Observable<Any>.never()
+    
+    observable.subscribe(onNext: { (element) in
+        print(element)
+    }, onCompleted: {
+        print("Completed")
+    })
+}
+
+example(of: "range") {
+    let observable = Observable<Int>.range(start: 1, count: 10)
+    
+    observable.subscribe(onNext: { i in
+        let n = Double(i)
+        
+        // Calculate the nth Fibonacci number
+        let fibonacci = Int(((pow(1.61803, n) - pow(0.61803, n)) / 2.23606).rounded())
+        print(fibonacci)
+    })
+}
+

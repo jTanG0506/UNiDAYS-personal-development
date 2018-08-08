8f237aa59801830e0e7aaa6b2f72dd13be35a2d2
Jonathan Tang <jonathan.tang@myunidays.com>
2018-08-08 14:09:44 +0100

    Complete challenge-two for Chapter 2 - Observables

---
+++ reactive-programing-with-swift/[c2-x]-observables/challenge-two/RxSwiftPlayground.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Please build the scheme 'RxSwiftPlayground' first
+import RxSwift
+
+example(of: "never") {
+  let observable = Observable<Any>.never()
+  let disposeBag = DisposeBag()
+  
+    observable.debug("observable", trimOutput: false)
+    .subscribe(
+      onNext: { element in
+        print(element)
+    },
+      onCompleted: {
+        print("Completed")
+    },
+      onDisposed: {
+        print("Disposed")
+    })
+    .disposed(by: disposeBag)
+}

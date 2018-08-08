493324c4c782ecb7ab5fd2866ed356295e3b48dd
Jonathan Tang <jonathan.tang@myunidays.com>
2018-08-08 11:23:49 +0100

    Create a Single trait that deals with reading the contents of a file

--- reactive-programing-with-swift/[c2]-observables/RxSwiftPlayground/RxSwiftPlayground.playground/Contents.swift
+++ reactive-programing-with-swift/[c2]-observables/RxSwiftPlayground/RxSwiftPlayground.playground/Contents.swift
@@ -106,3 +106,45 @@ example(of: "deferred") {
         print()
     }
 }
+
+example(of: "Single") {
+    // Single will emit either .success(value) or .error
+    let disposeBag = DisposeBag()
+    
+    enum FileReadError: Error {
+        case fileNotFound, unreadable, encodingFailed
+    }
+    
+    func loadText(from name: String) -> Single<String> {
+        return Single.create { single in
+            let disposable = Disposables.create()
+            
+            guard let path = Bundle.main.path(forResource: name, ofType: "txt") else {
+                single(.error(FileReadError.fileNotFound))
+                return disposable
+            }
+            
+            guard let data = FileManager.default.contents(atPath: path) else {
+                single(.error(FileReadError.unreadable))
+                return disposable
+            }
+            
+            guard let contents = String(data: data, encoding: .utf8) else {
+                single(.error(FileReadError.encodingFailed))
+                return disposable
+            }
+            
+            single(.success(contents))
+            return disposable
+        }
+    }
+    
+    loadText(from: "Copyright").subscribe {
+        switch $0 {
+        case .success(let string):
+            print(string)
+        case .error(let error):
+            print(error)
+        }
+    }.disposed(by: disposeBag)
+}

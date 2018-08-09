d67dd77ea510882c7307f2c6016105889d955f68
Jonathan Tang <jonathan.tang@myunidays.com>
2018-08-09 10:44:10 +0100

    Extend example on PublishSubjects regarding subscriptions after it has terminated

--- reactive-programing-with-swift/[c3]-subjects/RxSwiftPlayground/RxSwiftPlayground.playground/Contents.swift
+++ reactive-programing-with-swift/[c3]-subjects/RxSwiftPlayground/RxSwiftPlayground.playground/Contents.swift
@@ -4,14 +4,31 @@ import PlaygroundSupport
 PlaygroundPage.current.needsIndefiniteExecution = true
 
 // PublishSubjects only emit to current subscribers
+// PublishSubjects will re-emit its stop events to future subscribers.
 example(of: "PublishSubject") {
     let subject = PublishSubject<String>()
     subject.onNext("Is anyone listening?")
     
     let subscriptionOne = subject.subscribe(onNext: { string in
-        print(string)
+        print("S1 -", string)
     })
     
     subject.on(.next("1"))
     subject.onNext("2")
+    
+    let subscriptionTwo = subject.subscribe { string in
+        print("S2 -", string.element ?? string)
+    }
+    
+    subject.onNext("3")
+    subscriptionOne.dispose()
+    subject.onNext("4")
+    
+    subject.onCompleted()
+    subject.onNext("5")
+    subscriptionTwo.dispose()
+    let disposeBag = DisposeBag()
+    subject.subscribe {
+        print("S3 -", $0.element ?? $0)
+    }.disposed(by: disposeBag)
 }

1d35dd9166f18dbb3075d7c8bec0c91f66821771
Jonathan Tang <jonathan.tang@myunidays.com>
2018-08-10 12:54:35 +0100

    Create example for skipUntil

--- reactive-programing-with-swift/[c5]-filtering-operators/RxSwiftPlayground/RxSwiftPlayground.playground/Contents.swift
+++ reactive-programing-with-swift/[c5]-filtering-operators/RxSwiftPlayground/RxSwiftPlayground.playground/Contents.swift
@@ -58,3 +58,19 @@ example(of: "skipWhile") {
         print($0)
     }).disposed(by: disposeBag)
 }
+
+example(of: "skipUntil") {
+    let disposeBag = DisposeBag()
+    
+    let subject = PublishSubject<String>()
+    let trigger = PublishSubject<String>()
+    
+    subject.skipUntil(trigger).subscribe(onNext: {
+        print($0)
+    }).disposed(by: disposeBag)
+    
+    subject.onNext("A")
+    subject.onNext("B")
+    trigger.onNext("X")
+    subject.onNext("C")
+}

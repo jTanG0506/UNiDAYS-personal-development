3213f3c8850723f8c5b9bb027fb28ad4a4ae3ae3
Jonathan Tang <jonathan.tang@myunidays.com>
2018-08-10 12:31:06 +0100

    Create example for ignoreElements

---
+++ reactive-programing-with-swift/[c5]-filtering-operators/RxSwiftPlayground/RxSwiftPlayground.playground/Contents.swift
@@ -0,0 +1,15 @@
+import RxSwift
+
+example(of: "ignoreElements") {
+    let strikes = PublishSubject<String>()
+    let disposeBag = DisposeBag()
+    
+    strikes.ignoreElements().subscribe { _ in
+        print("You're out!")
+    }.disposed(by: disposeBag)
+    
+    strikes.onNext("X")
+    strikes.onNext("X")
+    strikes.onNext("X")
+    strikes.onCompleted()
+}

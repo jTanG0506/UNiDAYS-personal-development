6856fda1a23bc59cf20c79b26990d32d41c9f030
Jonathan Tang <jonathan.tang@myunidays.com>
2018-08-10 13:04:46 +0100

    Create example for takeUntil

--- reactive-programing-with-swift/[c5]-filtering-operators/RxSwiftPlayground/RxSwiftPlayground.playground/Contents.swift
+++ reactive-programing-with-swift/[c5]-filtering-operators/RxSwiftPlayground/RxSwiftPlayground.playground/Contents.swift
@@ -92,3 +92,19 @@ example(of: "takeWhile") {
         print($0)
     }).disposed(by: disposeBag)
 }
+
+example(of: "takeUntil") {
+    let disposeBag = DisposeBag()
+    
+    let subject = PublishSubject<String>()
+    let trigger = PublishSubject<String>()
+    
+    subject.takeUntil(trigger).subscribe(onNext: {
+        print($0)
+    }).disposed(by: disposeBag)
+    
+    subject.onNext("1")
+    subject.onNext("2")
+    trigger.onNext("X")
+    subject.onNext("3")
+}

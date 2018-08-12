a8bab820482eef0b3548f694273a85b486fe6a21
Jonathan Tang <jonathan.tang@myunidays.com>
2018-08-12 10:11:27 +0100

    Create example of map

--- reactive-programing-with-swift/[c7]-transforming-operators/RxSwiftPlayground/RxSwiftPlayground.playground/Contents.swift
+++ reactive-programing-with-swift/[c7]-transforming-operators/RxSwiftPlayground/RxSwiftPlayground.playground/Contents.swift
@@ -7,3 +7,16 @@ example(of: "toArray") {
         print($0)
     }).disposed(by: disposeBag)
 }
+
+example(of: "map") {
+    let disposeBag = DisposeBag()
+    
+    let formatter = NumberFormatter()
+    formatter.numberStyle = .spellOut
+    
+    Observable<NSNumber>.of(123, 4, 56).map {
+        formatter.string(from: $0) ?? ""
+    }.subscribe(onNext: {
+        print($0)
+    }).disposed(by: disposeBag)
+}

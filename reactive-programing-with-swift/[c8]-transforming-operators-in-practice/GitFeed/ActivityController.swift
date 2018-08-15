e62fbf1cd5163eae5c850fa18178e510ff8e6e2e
Jonathan Tang <jonathan.tang@myunidays.com>
2018-08-15 11:06:53 +0100

    Starter project for Chapter 8 - Transforming Operators in Practice

---
+++ reactive-programing-with-swift/[c8]-transforming-operators-in-practice/GitFeed/ActivityController.swift
@@ -0,0 +1,62 @@
+//
+//  AppDelegate.swift
+//  GitFeed
+//
+//  Created by Jonathan Tang on 15/08/2018.
+//  Copyright © 2018 Jonathan Tang. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+import Kingfisher
+
+class ActivityController: UITableViewController {
+    
+    private let repo = "ReactiveX/RxSwift"
+    
+    private let events = Variable<[Event]>([])
+    private let bag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = repo
+        
+        self.refreshControl = UIRefreshControl()
+        let refreshControl = self.refreshControl!
+        
+        refreshControl.backgroundColor = UIColor(white: 0.98, alpha: 1.0)
+        refreshControl.tintColor = UIColor.darkGray
+        refreshControl.attributedTitle = NSAttributedString(string: "Pull to refresh")
+        refreshControl.addTarget(self, action: #selector(refresh), for: .valueChanged)
+        
+        refresh()
+    }
+    
+    @objc func refresh() {
+        DispatchQueue.global(qos: .background).async { [weak self] in
+            guard let strongSelf = self else { return }
+            strongSelf.fetchEvents(repo: strongSelf.repo)
+        }
+    }
+    
+    func fetchEvents(repo: String) {
+        
+    }
+    
+    // MARK: - Table Data Source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return events.value.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let event = events.value[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")!
+        cell.textLabel?.text = event.name
+        cell.detailTextLabel?.text = event.repo + ", " + event.action.replacingOccurrences(of: "Event", with: "").lowercased()
+        cell.imageView?.kf.setImage(with: event.imageUrl, placeholder: UIImage(named: "blank-avatar"))
+        return cell
+    }
+}
+

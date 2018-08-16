ead7ee5f38ad8da24e08408e56c044501c68085e
Jonathan Tang <jonathan.tang@myunidays.com>
2018-08-16 20:49:55 +0100

    Add a Last-Modified header to the request

--- reactive-programing-with-swift/[c8]-transforming-operators-in-practice/GitFeed/ActivityController.swift
+++ reactive-programing-with-swift/[c8]-transforming-operators-in-practice/GitFeed/ActivityController.swift
@@ -21,6 +21,9 @@ class ActivityController: UITableViewController {
     private let repo = "ReactiveX/RxSwift"
     private let eventsFileURL = cachedFileURL("events.plist")
     
+    private let modifiedFileURL = cachedFileURL("modified.txt")
+    private let lastModified = Variable<NSString?>(nil)
+    
     private let events = Variable<[Event]>([])
     private let bag = DisposeBag()
     
@@ -38,6 +41,7 @@ class ActivityController: UITableViewController {
         
         let eventsArray = (NSArray(contentsOf: eventsFileURL)) as? [[String: Any]] ?? []
         events.value = eventsArray.compactMap(Event.init)
+        lastModified.value = try? NSString(contentsOf: modifiedFileURL, usedEncoding: nil)
         
         refresh()
     }
@@ -54,8 +58,12 @@ class ActivityController: UITableViewController {
             .map { urlString -> URL in
                 return URL(string: "https://api.github.com/repos/\(urlString)/events")!
             }
-            .map { url -> URLRequest in
-                return URLRequest(url: url)
+            .map { [weak self] url -> URLRequest in
+                var request = URLRequest(url: url)
+                if let modifiedHeader = self?.lastModified.value {
+                    request.addValue(modifiedHeader as String, forHTTPHeaderField: "Last-Modified")
+                }
+                return request
             }
             .flatMap { request -> Observable<(response: HTTPURLResponse, data: Data)> in
                 return URLSession.shared.rx.response(request: request)
@@ -75,6 +83,19 @@ class ActivityController: UITableViewController {
             return objects.compactMap(Event.init)
         }.subscribe(onNext: { [weak self] newEvents in
             self?.processEvents(newEvents)
+        }).disposed(by: bag)
+        
+        response.filter { response, _ in
+            return 200..<300 ~= response.statusCode
+        }.flatMap { response, _ -> Observable<NSString> in
+            guard let value = response.allHeaderFields["Last-Modified"] as? NSString else {
+                return Observable.empty()
+            }
+            return Observable.just(value)
+        }.subscribe(onNext: { [weak self] modifiedHeader in
+            guard let strongSelf = self else { return }
+            strongSelf.lastModified.value = modifiedHeader
+            try? modifiedHeader.write(to: strongSelf.modifiedFileURL, atomically: true, encoding: String.Encoding.utf8.rawValue)
         }).disposed(by: bag)
     }
     

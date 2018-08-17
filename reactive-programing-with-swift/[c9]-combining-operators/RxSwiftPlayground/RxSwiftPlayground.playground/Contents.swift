66757d6017998d8285d472236cf0313578636243
Jonathan Tang <jonathan.tang@myunidays.com>
2018-08-17 18:33:39 +0100

    Create example for Observable.concat

--- reactive-programing-with-swift/[c9]-combining-operators/RxSwiftPlayground/RxSwiftPlayground.playground/Contents.swift
+++ reactive-programing-with-swift/[c9]-combining-operators/RxSwiftPlayground/RxSwiftPlayground.playground/Contents.swift
@@ -11,3 +11,15 @@ example(of: "startWith") {
         print($0)
     }).disposed(by: bag)
 }
+
+example(of: "Observable.concat") {
+    let bag = DisposeBag()
+    
+    let first = Observable.of(1, 2, 3)
+    let second = Observable.of(4, 5, 6)
+    
+    let observable = Observable.concat([first, second])
+    observable.subscribe(onNext: {
+        print($0)
+    }).disposed(by: bag)
+}

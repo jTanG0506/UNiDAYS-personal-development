85475631bc6ca5b70cb84bec2c45f7d76654338b
Jonathan Tang <jonathan.tang@myunidays.com>
2018-08-21 09:45:18 +0100

    Create example for switchLatest

--- reactive-programing-with-swift/[c9]-combining-operators/RxSwiftPlayground/RxSwiftPlayground.playground/Contents.swift
+++ reactive-programing-with-swift/[c9]-combining-operators/RxSwiftPlayground/RxSwiftPlayground.playground/Contents.swift
@@ -189,3 +189,34 @@ example(of: "amb") {
     
     disposable.dispose()
 }
+
+example(of: "switchLatest") {
+    let one = PublishSubject<String>()
+    let two = PublishSubject<String>()
+    let three = PublishSubject<String>()
+    
+    let source = PublishSubject<Observable<String>>()
+    let observable = source.switchLatest()
+    
+    let disposable = observable.subscribe(onNext: {
+        print($0)
+    })
+    
+    source.onNext(one)
+    one.onNext("Some text from sequence one")
+    two.onNext("Some text from sequence two")
+    
+    source.onNext(two)
+    two.onNext("More text from sequence two")
+    one.onNext("and also from sequence one")
+    
+    source.onNext(three)
+    two.onNext("Why don't you see me?")
+    one.onNext("I'm alone, help me")
+    three.onNext("Hey it's three. I win.")
+    
+    source.onNext(one)
+    one.onNext("Nope. It's me, one!")
+    
+    disposable.dispose()
+}

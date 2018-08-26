2c0a19577d9bc6201e5e7f1fc80ab92f24562b62
Jonathan Tang <jonathan.tang@myunidays.com>
2018-08-26 18:09:43 +0100

    Load CSS in Electron Browser window

--- build-a-desktop-application-with-electron/HelloWorld/index.js
+++ build-a-desktop-application-with-electron/HelloWorld/index.js
@@ -3,6 +3,11 @@ const path = require('path');
 let mainWindow;
 
 app.on('ready', () => {
-  mainWindow = new BrowserWindow();
+  mainWindow = new BrowserWindow({
+    show: false
+  });
   mainWindow.loadURL(path.join('file://', __dirname, 'index.html'));
+  mainWindow.on('ready-to-show', () => {
+    mainWindow.show();
+  });
 })
